36b80c6258d08395df5817dabec7120acef8b3f2
nismo680 <github@nismo.ch>
2026-07-28 19:06:18 +0200

    edit Hotplug detecting

--- src/sessionflipper/events.py
+++ src/sessionflipper/events.py
@@ -9,7 +9,9 @@ class DisplayEvents:
 
         self._monitor = pyudev.Monitor.from_netlink(context)
         self._monitor.filter_by(subsystem="drm")
+        self._monitor.start()
 
     def wait(self) -> None:
-        for _device in self._monitor:
+        for device in self._monitor:
+            print(device.action, device.subsystem, dict(device))
             return
